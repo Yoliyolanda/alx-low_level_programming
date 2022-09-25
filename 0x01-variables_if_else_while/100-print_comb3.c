a9e96756756a8eb87aa81de21b40e9e376c44782
Yoliyolanda <yolandamugabe@gmail.com>
2022-09-25 13:16:48 -0700

    task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,12 +17,12 @@ for (num1 = 0; num1 < 9; num1++)
 {
 for (num2 = (num1 + 1); num2 <= 9; num2++)
 {
-putchar('0' + num1)
+putchar('0' + num1);
 putchar('0' + num2);
 if (num1 < 8)
 {
-putchar(',')
-putchar(' ')
+putchar(',');
+putchar(' ');
 }
 }
 }
