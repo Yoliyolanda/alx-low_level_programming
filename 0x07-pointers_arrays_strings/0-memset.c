74ebe6c2ea2dd5f554986830894a037a5576dce7
Yoliyolanda <yolandamugabe@gmail.com>
2022-09-26 06:05:29 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * _memset - fills a memory block wih a constant byte
- * @s: address to memory block
- * @b: char to be used
- * @n: number of bytes to be used
- *
- * Return: pointer to the memory block
+ * _memset - fills a memory wih a constant byte
+ * @s: source string
+ * @b: the contant byte for filling
+ * @n: length of buffer
+ * Return: new string
  */
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
-while (n)
+unsigned int i = 0;
+while (i < n)
 {
-s[n - 1] = b;
-n--;
+*(s + i) = b;
+i++;
 }
 return (s);
 }
