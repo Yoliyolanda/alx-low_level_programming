d8de0e583dcb1c6d322c133cd220edadac206897
Yoliyolanda <yolandamugabe@gmail.com>
2022-09-29 10:22:20 -0700

    task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -30,3 +30,18 @@ else
 check(s, start + 1, end - 1, flag);
 }
 }
+/**
+ * _strlen_recursion - calculates the length of a string
+ * @s: string to be used
+ * Return: length of the string
+ */
+int _strlen_recursion(char *s)
+{
+int sum = 0;
+if (*s != '\0')
+{
+sum++;
+sum += _strlen_recursion(s + 1);
+}
+return (sum);
+}
